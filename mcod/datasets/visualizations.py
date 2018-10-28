49701aeeae3d0a9c4783ec6d6a4cc3a1d1d386bd
alex <homaralex@gmail.com>
2018-10-28 02:21:43 +0200

    visualizations: add filter fns

--- mcod/datasets/visualizations.py
+++ mcod/datasets/visualizations.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def analyze_col(col):
     name = col.name
 
@@ -34,3 +37,57 @@ def analyze_df(df):
 
     return summaries
 
+
+COMPARISON_TYPES = ['=']
+
+
+def translate_comparison(comp_str_frontend):
+    """
+    Changes the comparison string/character sent from frontend into pandas query operator,
+    eg. '=' -> '=='
+    """
+    if comp_str_frontend == '=':
+        return '=='
+    return comp_str_frontend
+
+
+def filter_df(df, params):
+    # parse params
+    x_name = params['x']
+    y_name = params['y?']
+    aggr_operation = params['operation?']
+    filter_str = params['filter']
+
+    # parse and construct filter strings
+    # dict containing lists of pandas query strings for particular columns (keys)
+    col_filters = defaultdict(list)
+
+    for filter in filter_str.split(','):
+        comparison = None
+        # parse comparison type
+        for comp_type in COMPARISON_TYPES:
+            if comp_type in filter:
+                comparison = comp_type
+        if not comparison:
+            # could not parse comparison operation type - ignore this filter
+            continue
+
+        col_name, comp_val = filter.split(comparison)
+        # comparison unused for now
+        comparison = translate_comparison(comparison)
+        col_filters[col_name].append(comp_val)
+
+    # filter out data
+    for col_name, vals in col_filters.items():
+        df = df.loc[df[col_name].isin(vals)]
+
+    if aggr_operation:
+        if aggr_operation == 'unique':
+            df = df.groupby(x_name)[x_name].agg('count')
+            x = df.index.values
+            y = df.values
+    else:
+        x = df[x_name].values
+        y = df[y_name].values
+
+    return x, y
